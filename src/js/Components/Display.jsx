411b5f744dfbfedf812f7aa0e2c3f6f7efd98cdf
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-02-26 20:50:41 +0100

    feat: apply US formatter to display values

--- src/js/Components/Display.jsx
+++ src/js/Components/Display.jsx
@@ -1,9 +1,12 @@
 function Display({ currentValue, result }) {
+
+    const formatter = new Intl.NumberFormat('en-US');
+
     return (
         <>
             <div className="col p-0">
-                <p className="bg-white text-end p-3 m-0">{currentValue ? currentValue : 0}</p>
-                <p className="bg-white text-end p-3 m-0">total: {result ? result : 0}</p>
+                <p className="bg-white text-end p-3 m-0">{currentValue ? formatter.format(currentValue) : 0}</p>
+                <p className="bg-white text-end p-3 m-0">total: {result ? formatter.format(result) : 0}</p>
             </div>
         </>
     )
