88bdbedc982ae150c457e7d68254cd6283db1747
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-05 18:30:36 +0100

    fix: preserve decimal point in formatted display

--- src/js/Components/Display.jsx
+++ src/js/Components/Display.jsx
@@ -1,11 +1,20 @@
 function Display({ currentValue, result }) {
 
     const formatter = new Intl.NumberFormat('en-US');
+    
+    let formattedCurrentValue ;
+
+    if (typeof currentValue === 'string') {
+        const integerDigits = currentValue.split('').filter((_, i, arr) => i < arr.length - 1);
+        formattedCurrentValue = formatter.format(Number(integerDigits.join(''))) + '.';
+    } else {
+        formattedCurrentValue = formatter.format(currentValue);
+    }
 
     return (
         <>
             <div className="col p-0">
-                <p className="bg-white text-end p-3 m-0">{currentValue ? formatter.format(currentValue) : 0}</p>
+                <p className="bg-white text-end p-3 m-0">{currentValue ?  formattedCurrentValue : 0}</p>
                 <p className="bg-white text-end p-3 m-0">total: {result ? formatter.format(result) : 0}</p>
             </div>
         </>
